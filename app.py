d14bdeba32afea2a0cbd6ce31c9e1627931236a4
AmanJ4588 <amanj4588@gmail.com>
2024-07-08 17:14:24 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+
+# Store chat history
+chat_history = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', chat_history=chat_history)
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    user_message = request.form['message']
+    # Here you can process the user message and generate a response
+    bot_response = f"Echo: {user_message}"
+    chat_history.append({'user': user_message, 'bot': bot_response})
+    return jsonify(chat_history=chat_history)
+
+if __name__ == '__main__':
+    app.run(debug=True)
