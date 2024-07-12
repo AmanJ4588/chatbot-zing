b2a9b6361fd3435c7205fecc8c1af8995a59e206
AmanJ4588 <75908756+AmanJ4588@users.noreply.github.com>
2024-07-12 14:24:21 +0530

    Deleted firebaseConnectivity.py (no use)

--- firebaseConnectivity.py
+++
@@ -1,7 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import auth
-from firebase_admin import db
-
-cred = credentials.Certificate("C:\Users\Aman\Downloads\chatbot-zing-e77df-firebase-adminsdk-nelvr-66994cf765.json")
-firebase_admin.initialize_app(cred)
